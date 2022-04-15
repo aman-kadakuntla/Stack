5250f5298bbac9460e8846f885ba7a4ca17bccdd
Suresh Babu <kadakuntlaaman@gmail.com>
2022-04-15 13:49:22 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackOperations stackOperations = new StackOperations(10);
+            stackOperations.show();
+            stackOperations.push(3);
+            stackOperations.push(4);
+            stackOperations.push(5);
+            stackOperations.show();
+            stackOperations.pop();
+            stackOperations.show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StackOperations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    internal class StackOperations
+    {
+        static int top;
+        static int[] arr;
+        public StackOperations(int capacity)
+        {
+            arr = new int[capacity];
+            top = -1;
+        }
+        public void push(int a)
+        {
+            if (top == arr.Length - 1)
+                Console.WriteLine("Stack is full");
+            else
+            {
+                arr[++top] = a;
+            }
+        }
+        public void pop()
+        {
+            if (top == -1)
+                Console.WriteLine("Stack is empty");
+            else
+                top--;
+        }
+        public void pop(int NumberOfElementsToBeDeleted)
+        {
+            if (top == -1)
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                for (int i = 0; i < NumberOfElementsToBeDeleted; i++)
+                    top--;
+            }
+        }
+        public void show()
+        {
+            if (top == -1)
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
